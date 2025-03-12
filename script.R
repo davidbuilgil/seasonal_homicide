ea2af19cb88bdabd997b7af0d509adc96e33c557
David Buil Gil <david.builgil@manchester.ac.uk>
2025-03-12 14:12:30 +0000

    upload data

---
+++ script.R
@@ -0,0 +1,12 @@
+
+# Load packages
+library(here)
+library(dplyr)
+
+# Load precipitation data
+precipitacion1 <- read.csv(here("data/col-rainfall-adm2-part1.csv"))
+precipitacion2 <- read.csv(here("data/col-rainfall-adm2-part2.csv"))
+precipitacion3 <- read.csv(here("data/col-rainfall-adm2-part3.csv"))
+
+
+
