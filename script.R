92b4eebb498c9fe7fc386dd315d0c4bb64d6e306
David Buil Gil <david.builgil@manchester.ac.uk>
2025-03-21 15:35:25 +0000

    maps done

--- script.R
+++ script.R
@@ -7,6 +7,8 @@ library(here)
 library(ggplot2)
 library(plm)
 library(nlme)
+library(sf)
+library(spdep)
 library(stringr)
 library(ggpubr)
 library(lubridate)
@@ -146,6 +148,12 @@ rainfall <- rainfall %>%
          COD_MUNI = gsub("^0", "", gsub("^CO", "", ADM2_PCODE)),
          COD_MUNI = as.integer(COD_MUNI))
 
+# Aggregate rainfall data by department and monthly
+rainfall_dep_month <- rainfall %>%
+  group_by(month, year, COD_DEPTO) %>%
+  summarise(rfh = sum(rfh)) %>%
+  mutate(month_name = factor(month.name[month], levels = month.name))
+
 # Aggregate rainfall data by municipality and monthly
 rainfall_muni_month <- rainfall %>%
   group_by(month, year, COD_MUNI) %>%
@@ -165,6 +173,12 @@ rainfall_muni_month <- rainfall_muni_month %>%
     deviation = rfh - mean_rfh,  # Deviation size
     above_below_avg = ifelse(deviation > 0, "Above", "Below")
   )
+
+# Aggregate homicide data by department and monthly
+homicide_dep_month <- homicide %>%
+  group_by(month, year, COD_DEPTO) %>%
+  summarise(homicide = sum(CANTIDAD, na.rm = TRUE)) %>%
+  mutate(month_name = factor(month.name[month], levels = month.name))
 
 # Aggregate homicide data by municipality and monthly
 homicide_muni_month <- homicide %>%
@@ -236,25 +250,21 @@ fe_int_dept_model <- plm(scaled_homicide_deviation ~ scaled_rainfall_deviation *
 
 #saveRDS(fe_int_dept_model, here("output/fe_int_dept_model.rds"))
 #fe_int_dept_model <- readRDS(here("output/fe_int_dept_model.rds"))
-summary(fe_int_dept_model)
+#summary(fe_int_dept_model)
 
 # Extract coefficients table
-coeff_fe_int_table <- as.data.frame(coef(summary(fe_int_dept_model)))
+#coeff_fe_int_table <- as.data.frame(coef(summary(fe_int_dept_model)))
 
 # Filter for COD_MUNI interaction terms only
-coeff_fe_int_table <- coeff_fe_int_table[grep("scaled_rainfall_deviation:factor\\(COD_MUNI\\)", rownames(coeff_fe_int_table)), ]
+#coeff_fe_int_table <- coeff_fe_int_table[grep("scaled_rainfall_deviation:factor\\(COD_MUNI\\)", rownames(coeff_fe_int_table)), ]
 
 # COD_MUNI identifiers
-coeff_fe_int_table$COD_MUNI <- gsub("scaled_rainfall_deviation:factor\\(COD_MUNI\\)", "", rownames(coeff_fe_int_table))
-coeff_fe_int_table <- coeff_fe_int_table %>%
-  mutate(COD_MUNI = as.integer(COD_MUNI))
+#coeff_fe_int_table$COD_MUNI <- gsub("scaled_rainfall_deviation:factor\\(COD_MUNI\\)", "", rownames(coeff_fe_int_table))
+#coeff_fe_int_table <- coeff_fe_int_table %>%
+#  mutate(COD_MUNI = as.integer(COD_MUNI))
 
-
-
-
-
-library(sf)
-library(spdep)
+#write.csv(coeff_fe_int_table, here("output/coeff_fe_int_table.csv"))
+coeff_fe_int_table <- read.csv(here("output/coeff_fe_int_table.csv"))
 
 # Load shapefile data
 shapefile_data <- st_read(here("data/Municipios_USAID/Municipios_USAID.shp"))
@@ -262,7 +272,120 @@ shapefile_data <- st_read(here("data/Municipios_USAID/Municipios_USAID.shp"))
 # Check CRS
 #st_crs(shapefile_data)
 
-# Select only variables of interest and extract centroids from polygons
+# Aggregate polygons by DPTO_CCDGO
+#department_polygons <- shapefile_data %>%
+#  group_by(DPTO_CCDGO) %>%
+#  summarise(geometry = st_union(geometry)) %>%
+#  ungroup()
+
+# Optional: Save the new shapefile
+#st_write(department_polygons, here("data/departamentos/Departamento_Polygons.shp"))
+
+# Load departments shapefile data
+department_polygons <- st_read(here("data/departamentos/Departamento_Polygons.shp"))
+
+# Calculate monthly mean and deviantion from department and monthly mean across years
+# Step 1: Calculate mean rfh for each month across years
+rainfall_dep_monthly_mean <- rainfall_dep_month %>%
+  group_by(month, COD_DEPTO) %>%
+  summarise(mean_rfh = mean(rfh, na.rm = TRUE))
+
+# Step 2: Identify deviation from the mean and direction
+rainfall_dep_month <- rainfall_dep_month %>%
+  left_join(rainfall_dep_monthly_mean, by = c("month", "COD_DEPTO")) %>%
+  mutate(
+    deviation = rfh - mean_rfh,  # Deviation size
+    above_below_avg = ifelse(deviation > 0, "Above", "Below")
+  )
+
+# List of months, years and departments from rainfall data
+dep_month <- rainfall_dep_month %>%
+  dplyr::select(month, year, COD_DEPTO) %>%
+  filter(year >= 2003)
+
+# Left join homicide aggregates to list of months, years and departments
+# Assume 0 (NA) if is no homicide recorded
+homicide_dep_month <- dep_month %>%
+  left_join(homicide_dep_month, by = c("month", "year", "COD_DEPTO")) %>%
+  mutate(homicide = ifelse(is.na(homicide), 0, homicide),
+         month_name = factor(month.name[month], levels = month.name))
+
+# Standardize homicide counts within each year
+homicide_dep_month <- homicide_dep_month %>%
+  group_by(year) %>%
+  mutate(standardized_homicide = scale(homicide)[, 1]) %>%
+  ungroup()
+
+# Calculate department and monthly homicide mean (after standardization)
+homicide_dep_monthly_mean <- homicide_dep_month %>%
+  group_by(month, COD_DEPTO) %>%
+  summarise(mean_standardized_homicide = mean(standardized_homicide, na.rm = TRUE))
+
+# Calculate homicide deviation from mean for each department and month
+homicide_dep_month <- homicide_dep_month %>%
+  left_join(homicide_dep_monthly_mean, by = c("month", "COD_DEPTO")) %>%
+  mutate(deviation = standardized_homicide - mean_standardized_homicide)
+
+# Merge rainfall and homicide department data
+merged_dep_data <- rainfall_dep_month %>%
+  inner_join(homicide_dep_month, by = c("year", "month", "COD_DEPTO")) %>%
+  select(year, month, COD_DEPTO, rfh, mean_rfh, deviation.x, 
+         standardized_homicide, mean_standardized_homicide, deviation.y) %>%
+  rename(rainfall_deviation = deviation.x,
+         homicide_deviation = deviation.y)
+
+# Calculate mean rainfall and homicide deviations for last 3 years (random number of years for visualization)
+merged_dep_data_3y <- merged_dep_data %>%
+  filter(year >= 2022 & year < 2025) %>%
+  group_by(COD_DEPTO, month) %>%
+  summarise(
+    mean_rainfall_deviation = mean(rainfall_deviation, na.rm = TRUE),
+    mean_rainfall = mean(rfh, na.rm = TRUE),
+    monthly_mean_rainfall_all = mean(mean_rfh, na.rm = TRUE),
+    mean_homicide_deviation = mean(homicide_deviation, na.rm = TRUE),
+    mean_homicide = mean(standardized_homicide, na.rm = TRUE),
+    monthly_mean_homicide_all = mean(mean_standardized_homicide, na.rm = TRUE)
+  )
+
+# Convert DPTO_CCDGO to integer, removing leading zeros
+department_polygons$COD_DEPTO <- as.integer(department_polygons$DPTO_CCDGO)
+
+# Merge shapefile with 3-year mean rainfall and homicide data
+merged_dep_data_3y <- merged_dep_data_3y %>%
+  full_join(department_polygons, by = "COD_DEPTO") %>%
+  mutate(month_name = factor(month.name[month], levels = month.name))
+
+# Reassign the sf class after join
+merged_dep_data_3y <- st_as_sf(merged_dep_data_3y, crs = st_crs(department_polygons))
+
+# Plot 3 year average homicide deviation by department
+homicide_map_dep <- ggplot(data = merged_dep_data_3y) +
+  geom_sf(aes(fill = mean_homicide_deviation), colour = NA) +
+  scale_fill_gradient2(
+    low = "darkblue",    # Colour for negative values
+    mid = "lightgrey",     # Neutral (zero) point
+    high = "darkred",   # Colour for positive values
+    midpoint = 0,      # Ensures zero is the neutral point
+    #limits = c(-2, 2),
+    na.value = "white",
+    guide = guide_colourbar(title = NULL),
+    labels = scales::number_format(accuracy = 0.1)
+  ) +
+  facet_wrap(~month_name, ncol = 4) +
+  labs(
+    title = "Mean Homicide Deviation (2022-2024)",
+    fill = ""
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text = element_blank(),
+    axis.title = element_blank(),
+    panel.grid = element_blank(),
+    strip.text = element_text(size = 10, face = "bold"),
+    legend.position = "bottom"
+  )
+
+# At municipal level, select only variables of interest and extract centroids from polygons
 shapefile_data <- shapefile_data %>%
   mutate(COD_MUNI = as.integer(MPIO_CCDGO)) %>%
   dplyr::select(COD_MUNI, MPIO_CNMBR) %>%
@@ -271,7 +394,7 @@ shapefile_data <- shapefile_data %>%
 
 # Calculate mean rainfall and homicide deviatios for last 3 years (random number of years for visualization)
 merged_muni_data_3y <- merged_muni_data %>%
-  filter(year >= 2020 & year < 2025) %>%
+  filter(year >= 2022 & year < 2025) %>%
   group_by(COD_MUNI, month) %>%
   summarise(
     mean_rainfall_deviation = mean(rainfall_deviation, na.rm = TRUE),
@@ -280,10 +403,6 @@ merged_muni_data_3y <- merged_muni_data %>%
     mean_homicide_deviation = mean(homicide_deviation, na.rm = TRUE),
     mean_homicide = mean(standardized_homicide, na.rm = TRUE),
     monthly_mean_homicide_all = mean(mean_standardized_homicide, na.rm = TRUE)
-  ) %>%
-  mutate(
-    mean_rainfall_relative_dif = (mean_rainfall - monthly_mean_rainfall_all) / monthly_mean_rainfall_all * 100,
-    mean_homicide_relative_dif = (mean_homicide - monthly_mean_homicide_all) / monthly_mean_homicide_all * 100
   )
 
 # Removing datasets from environment
@@ -292,22 +411,24 @@ rm(fe_int_dept_model, homicide, homicide_month, homicide_monthly_mean,
    rainfall, rainfall_month, rainfall_muni_month, rainfall_muni_monthly_mean,
    rainfall_plot, rainfall_monthly_mean)
    
-# Merge shapefile with 3-year mean rainfall and homicide data
+# Merge shapefile with 5-year mean rainfall and homicide data
 merged_muni_data_3y <- merged_muni_data_3y %>%
   full_join(shapefile_data, by = "COD_MUNI")
 
 # Also link coefficients from fixed effect model
 merged_muni_data_3y <- merged_muni_data_3y %>%
   left_join(coeff_fe_int_table, by = "COD_MUNI") %>%
-  mutate(month_name = factor(month.name[month], levels = month.name))
+  mutate(month_name = factor(month.name[month], levels = month.name)) %>%
+  dplyr::select(-X) %>%
+  filter(!is.na(month))
 
 # Reassign the sf class after join
 merged_muni_data_3y <- st_as_sf(merged_muni_data_3y, crs = st_crs(shapefile_data))
 
 # New data to plot regression estimates
 merged_muni_data_slopes <- merged_muni_data_3y %>%
-  dplyr::select(COD_MUNI, MPIO_CNMBR, geometry, Estimate, `Std. Error`,
-                `Pr(>|t|)`) %>%
+  dplyr::select(COD_MUNI, MPIO_CNMBR, geometry, Estimate, Std..Error,
+                Pr...t..) %>%
   unique()
 
 # Plot regression estimates
@@ -335,174 +456,8 @@ ggsave(here('output/estimates_map.jpg'), width = 7, height = 7)
 merged_muni_data_3y <- merged_muni_data_3y %>%
   st_simplify(dTolerance = 0.01) 
 
-# Create spatial weights matrix (Queen contiguity)
-#nb <- poly2nb(merged_muni_data_3y %>% filter(month == 1)) 
-nb <- poly2nb(merged_muni_data_3y) 
-lw <- nb2listw(nb, style = "W", zero.policy = TRUE)
-
-# Compute LISA for rainfall deviation
-lisa_rainfall <- localmoran(merged_muni_data_3y$mean_rainfall_deviation, lw, zero.policy = TRUE)
-
-# Add LISA values to dataset
-merged_muni_data_3y <- merged_muni_data_3y %>%
-  mutate(
-    moran_I_rain = lisa_rainfall[, 1],
-    p_value_rain = lisa_rainfall[, 5]
-  )
-
-# Compute LISA for rainfall deviation each month
-merged_muni_data_3y <- merged_muni_data_3y %>%
-  filter(!is.na(month)) %>%
-  mutate(
-    cluster_rain = case_when(
-      moran_I_rain > 0 & p_value_rain <= 0.05 ~ "High-High (Hotspot)",
-      moran_I_rain < 0 & p_value_rain <= 0.05 ~ "Low-Low (Coldspot)",
-      moran_I_rain > 0 & p_value_rain > 0.05 ~ "Non-significant High",
-      moran_I_rain < 0 & p_value_rain > 0.05 ~ "Non-significant Low",
-      TRUE ~ "Non-significant"
-    )
-  )
-
-# LISA for Homicide
-lisa_homicide <- localmoran(merged_muni_data_3y$mean_homicide_deviation, lw, zero.policy = TRUE)
-
-merged_muni_data_3y <- merged_muni_data_3y %>%
-  mutate(
-    moran_I_homicide = lisa_homicide[, 1],
-    p_value_homicide = lisa_homicide[, 5]
-  )
-
-# Assign clusters using global thresholds
-merged_muni_data_3y <- merged_muni_data_3y %>%
-  filter(!is.na(month)) %>%
-  mutate(
-    cluster_homicide = case_when(
-      moran_I_homicide > 0 & p_value_homicide <= 0.05 ~ "High-High (Hotspot)",
-      moran_I_homicide < 0 & p_value_homicide <= 0.05 ~ "Low-Low (Coldspot)",
-      moran_I_homicide > 0 & p_value_homicide > 0.05 ~ "Non-significant High",
-      moran_I_homicide < 0 & p_value_homicide > 0.05 ~ "Non-significant Low",
-      TRUE ~ "Non-significant"
-    )
-  )
-
-# Compute LISA for rainfall deviation each month
-#lisa_results_rainfall <- merged_muni_data_3y %>%
-#  filter(!is.na(month)) %>%
-#  group_by(month) %>%
-#  mutate(
-#    local_moran_I_rain = localmoran(mean_rainfall_deviation, lw, zero.policy = TRUE)[, 1],
-#    p_value_rain = localmoran(mean_rainfall_deviation, lw, zero.policy = TRUE)[, 5],
-#    cluster_rain = case_when(
-#      local_moran_I_rain > 0 & p_value_rain <= 0.05 ~ "High-High (Hotspot)",
-#      local_moran_I_rain < 0 & p_value_rain <= 0.05 ~ "Low-Low (Coldspot)",
-#      local_moran_I_rain > 0 & p_value_rain > 0.05 ~ "Non-significant High",
-#      local_moran_I_rain < 0 & p_value_rain > 0.05 ~ "Non-significant Low",
-#      TRUE ~ "Non-significant"
-#    )
-#  ) %>%
-#  ungroup() %>%
-#  dplyr::select(COD_MUNI, month, local_moran_I_rain, p_value_rain, cluster_rain) %>%
-#  st_drop_geometry()
-
-# Compute LISA for homicide deviation each month
-#lisa_results_homicide <- merged_muni_data_3y %>%
-#  filter(!is.na(month)) %>%
-#  group_by(month) %>%
-#  mutate(
-#    local_moran_I_homicide = localmoran(mean_homicide_deviation, lw, zero.policy = TRUE)[, 1],
-#    p_value_homicide = localmoran(mean_homicide_deviation, lw, zero.policy = TRUE)[, 5],
-#    cluster_homicide = case_when(
-#      local_moran_I_homicide > 0 & p_value_homicide <= 0.05 ~ "High-High (Hotspot)",
-#      local_moran_I_homicide < 0 & p_value_homicide <= 0.05 ~ "Low-Low (Coldspot)",
-#      local_moran_I_homicide > 0 & p_value_homicide > 0.05 ~ "Non-significant High",
-#      local_moran_I_homicide < 0 & p_value_homicide > 0.05 ~ "Non-significant Low",
-#      TRUE ~ "Non-significant"
-#    )
-#  ) %>%
-#  ungroup() %>%
-#  dplyr::select(COD_MUNI, month, local_moran_I_homicide, p_value_homicide, cluster_homicide) %>%
-#  st_drop_geometry()
-
-# Left join LISA results
-#merged_muni_data_3y <- merged_muni_data_3y %>%
-#  filter(!is.na(month)) %>%
-#  left_join(lisa_results_rainfall, by = c("COD_MUNI", "month")) %>%
-#  left_join(lisa_results_homicide, by = c("COD_MUNI", "month"))
-
-# Plot 3 year average homicide deviation by municipality
-homicide_map <- ggplot(merged_muni_data_3y) +
-  geom_sf(aes(fill = cluster_homicide), colour = NA) +
-  scale_fill_manual(
-    values = c(
-      "High-High (Hotspot)" = "red",
-      "Low-Low (Coldspot)" = "blue",
-      "Non-significant High" = "pink",
-      "Non-significant Low" = "lightblue",
-      "Non-significant" = "grey80"
-    )
-  ) +  
-  facet_wrap(~month, ncol = 4) +
-  labs(
-    title = "LISA Homicide Deviation Clusters by Month",
-    fill = "Cluster Type"
-  ) +
-  theme_minimal() +
-  theme(
-    axis.text = element_blank(),
-    axis.title = element_blank(),
-    panel.grid = element_blank(),
-    strip.text = element_text(size = 10, face = "bold"),
-    legend.position = "bottom"
-  )
-
 # Plot 3 year average rainfall deviation by municipality
-rainfall_map <- ggplot(merged_muni_data_3y) +
-  geom_sf(aes(fill = cluster_rain), colour = NA) +
-  scale_fill_manual(
-    values = c(
-      "High-High (Hotspot)" = "red",
-      "Low-Low (Coldspot)" = "blue",
-      "Non-significant High" = "pink",
-      "Non-significant Low" = "lightblue",
-      "Non-significant" = "grey80"
-    )
-  ) +  
-  facet_wrap(~month, ncol = 4) +
-  labs(
-    title = "LISA Rainfall Deviation Clusters by Month",
-    fill = "Cluster Type"
-  ) +
-  theme_minimal() +
-  theme(
-    axis.text = element_blank(),
-    axis.title = element_blank(),
-    panel.grid = element_blank(),
-    strip.text = element_text(size = 10, face = "bold"),
-    legend.position = "bottom"
-  )
-
-# Plot two maps together
-ggarrange(rainfall_map, homicide_map, common.legend = TRUE, legend="bottom")
-ggsave(here('output/rainfall_homicide_LISA_maps.jpg'), width = 10, height = 7)
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-# Plot 3 year average rainfall deviation by municipality
-rainfall_map <- ggplot(data = merged_muni_data_3y %>% filter(!is.na(month))) +
+rainfall_map <- ggplot(data = merged_muni_data_3y) +
   geom_sf(aes(fill = mean_rainfall_deviation), colour = NA) +
   scale_fill_gradient2(
     low = "darkblue",      # Color for negative values
@@ -514,32 +469,7 @@ rainfall_map <- ggplot(data = merged_muni_data_3y %>% filter(!is.na(month))) +
   ) +
   facet_wrap(~month_name, ncol = 4) +
   labs(
-    title = "Mean Rainfall Deviation (2023-2024)",
-    fill = ""
-  ) +
-  theme_minimal() +
-  theme(
-    axis.text = element_blank(),
-    axis.title = element_blank(),
-    panel.grid = element_blank(),
-    strip.text = element_text(size = 10, face = "bold"),
-    legend.position = "bottom"
-  )
-
-# Plot 3 year average homicide deviation by municipality
-homicide_map <- ggplot(data = merged_muni_data_3y %>% filter(!is.na(month))) +
-  geom_sf(aes(fill = mean_homicide_deviation), colour = NA) +
-  scale_fill_gradient2(
-    low = "darkblue",    # Colour for negative values
-    mid = "lightgrey",     # Neutral (zero) point
-    high = "darkred",   # Colour for positive values
-    midpoint = 0,      # Ensures zero is the neutral point
-    na.value = "white",
-    guide = guide_colourbar(title = NULL)
-  ) +
-  facet_wrap(~month_name, ncol = 4) +
-  labs(
-    title = "Mean Homicide Deviation (2023-2024)",
+    title = "Mean RFH Deviation (2022-2024)",
     fill = ""
   ) +
   theme_minimal() +
@@ -552,12 +482,18 @@ homicide_map <- ggplot(data = merged_muni_data_3y %>% filter(!is.na(month))) +
   )
 
 # Plot two maps together
-ggarrange(rainfall_map, homicide_map)
-ggsave(here('output/rainfall_homicide_maps.jpg'), width = 10, height = 7)
+ggarrange(rainfall_map, homicide_map_dep)
+ggsave(here('output/rainfall_homicide_maps_final.jpg'), width = 11, height = 7)
 
-
-
-
+# Plot three maps together
+ggarrange(
+  rainfall_map, homicide_map_dep,
+  estimates_map,
+  ncol = 2, nrow = 2,
+  widths = c(1, 1),
+  heights = c(1, 0.6)
+)
+ggsave(here('output/maps_final.jpg'), width = 10, height = 12)
 
 
 
